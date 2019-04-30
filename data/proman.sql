073c475ea0a52c3d69b71437ae5b747ab151ef5a
Gergely Simonics <99gergely99@gmail.com>
2019-04-30 09:31:53 +0200

    .sql fixed

--- data/proman.sql
+++ data/proman.sql
@@ -34,12 +34,10 @@ DROP SEQUENCE IF EXISTS public.cards_id_seq;
 CREATE TABLE cards (
     id serial NOT NULL,
     name varchar,
-    board_id integer NOT NULL,
     status_id integer NOT NULL
 
 );
 
-DROP TABLE IF EXISTS public.board_status_card;
 
 ALTER TABLE ONLY boards
     ADD CONSTRAINT pk_boards_id PRIMARY KEY (id);
@@ -51,35 +49,38 @@ ALTER TABLE ONLY cards
     ADD CONSTRAINT pk_cards_id PRIMARY KEY (id);
 
 
--- ALTER TABLE ONLY question_tag
---     ADD CONSTRAINT pk_question_tag_id PRIMARY KEY (question_id, tag_id);
-
-
-ALTER TABLE ONLY status
+ALTER TABLE ONLY statuses
     ADD CONSTRAINT fk_boards_id FOREIGN KEY (board_id) REFERENCES boards(id);
 
-ALTER TABLE ONLY card
+ALTER TABLE ONLY cards
     ADD CONSTRAINT fk_statuses_id FOREIGN KEY (status_id) REFERENCES statuses(id);
-
-ALTER TABLE ONLY card
-    ADD CONSTRAINT fk_boards_id FOREIGN KEY (board_id) REFERENCES boards(id);
-
 
 
 INSERT INTO boards VALUES (1, 'proMan projekt');
 INSERT INTO boards VALUES (2, 'masik projekt');
-SELECT pg_catalog.setval('boards_id_seq', 2, true);
+INSERT INTO boards VALUES (3, 'harmadik tabla');
+INSERT INTO boards VALUES (4, 'negyedik tabla');
+INSERT INTO boards VALUES (5, 'ötödik üres tábla ékezetekkeláéíóőúöüű');
+SELECT pg_catalog.setval('boards_id_seq', 5, true);
 
 INSERT INTO statuses VALUES (1, 'new', 1);
-INSERT INTO statuses VALUES (2, 'to do',1);
-INSERT INTO statuses VALUES (3, 'in progress', 1);
-INSERT INTO statuses VALUES (4, 'done', 2);
-SELECT pg_catalog.setval('statuses_id_seq', 4, true);
+INSERT INTO statuses VALUES (2, 'to do',2);
+INSERT INTO statuses VALUES (3, 'in progress', 3);
+INSERT INTO statuses VALUES (4, 'in progress', 3);
+INSERT INTO statuses VALUES (5, 'done', 4);
+INSERT INTO statuses VALUES (6, 'done2', 5);
+INSERT INTO statuses VALUES (7, 'done3', 5);
+INSERT INTO statuses VALUES (8, 'done4', 4);
+INSERT INTO statuses VALUES (9, 'done5', 3);
+SELECT pg_catalog.setval('statuses_id_seq', 2, true);
 
-INSERT INTO cards VALUES (1, 'environment', 1, 1);
-INSERT INTO cards VALUES (2, 'something', 1, 1);
-INSERT INTO cards VALUES (3, 'something1', 1, 2);
-INSERT INTO cards VALUES (4, 'something2', 1, 3);
-INSERT INTO cards VALUES (5, 'something3', 2, 1);
-INSERT INTO cards VALUES (6, 'something4',2 ,1);
-SELECT pg_catalog.setval('cards_id_seq', 6, true);
+INSERT INTO cards VALUES (1, 'environment', 1);
+INSERT INTO cards VALUES (2, 'something', 2);
+INSERT INTO cards VALUES (3, 'something1', 3);
+INSERT INTO cards VALUES (4, 'something2', 4);
+INSERT INTO cards VALUES (5, 'something3', 5);
+INSERT INTO cards VALUES (6, 'something4', 6);
+INSERT INTO cards VALUES (7, 'something4', 7);
+INSERT INTO cards VALUES (8, 'something4', 8);
+SELECT pg_catalog.setval('cards_id_seq', 8, true);
+
